8f2b4302845337ab5645c583c5c808050e58e61e
Nguyễn Lý Quang Hào <71426649+haonguyen0110@users.noreply.github.com>
2021-11-25 15:28:51 +0700

    Add files via upload

---
+++ javascript/index.js
@@ -0,0 +1,19 @@
+const contact = () => document.getElementById('contact').scrollIntoView({
+    behavior: 'smooth'
+});
+const project = () => document.getElementById('project').scrollIntoView({
+    behavior: 'smooth'
+});
+const about = () => document.getElementById('company').scrollIntoView({
+    behavior: 'smooth'
+});
+const home = () => document.getElementById('header').scrollIntoView({
+    behavior: 'smooth'
+});
+const work = () => document.getElementById('work').scrollIntoView({
+    behavior: 'smooth'
+});
+const commit = () => document.getElementById('commit').scrollIntoView({
+    behavior: 'smooth'
+});
+
